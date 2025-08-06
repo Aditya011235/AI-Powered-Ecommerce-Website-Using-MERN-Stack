8e864cf10d9571422061d1bb4e9c7ba9b27a0e35
aditya kumar <adityakumarpatika6@gmail.com>
2025-08-07 02:45:44 +0530

    backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 const connectDb = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.log("MongoDB connection failed:");
-    process.exit(1); // Exit the process with failure
-  }
-};
-export default connectDb;
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("DB connected")
+    } catch (error) {
+        console.log("DB error")
+    }
+    
+}
+export default connectDb
--- backend/model/UserModel.js
+++ backend/model/UserModel.js
@@ -1,25 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+    name:{
+        type:String,
+        required:true
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
+    email:{
+        type:String,
+        required:true,
+        unique:true
     },
-    password: {
-        type: String,
-        
+    password:{
+        type:String
     },
     cartData:{
-        type: Object,
-        default: {},
-    },
-},{timestamp : true , minimize : false})
+        type:Object,
+        default:{}
+    }
+},{timestamps:true , minimize:false})
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User",userSchema)
 
-export default User;
+export default User
